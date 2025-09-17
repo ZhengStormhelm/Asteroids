78d7897a4e9cdea4ea312c9307a551636014ce82
Zheng <github.zh@stormhelm.com>
2025-09-17 20:02:38 +0800

    B: Add the clock

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import pygame
 from constants import *
 
-pygame.init()
-
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +14,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
