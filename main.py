5f74378cfca98aa5710ff97e08393924da15602b
Zheng <github.zh@stormhelm.com>
2025-09-18 16:57:46 +0800

    C: Add Rotation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -11,9 +13,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+                
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
