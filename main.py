fd3d1584520435fb22958b51f766d8cdf6f9b564
Zheng <github.zh@stormhelm.com>
2025-09-19 08:32:51 +0800

    H: Add Splitting

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         for thing in drawable:
